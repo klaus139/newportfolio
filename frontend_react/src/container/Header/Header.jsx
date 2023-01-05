941f17b8d5265c72006b39b3e9ba18901dc5174a
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2023-01-05 15:01:49 +0100

    implemented navbar and header components

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,9 +1,73 @@
 import React from 'react';
 import './Header.scss';
+import { motion } from 'framer-motion';
+import { images } from '../../constants';
+
+const scaleVariant = {
+  whileInView:{
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut'
+    }
+  }
+}
 
 const Header = () => {
+  
   return (
-    <div>Header</div>
+    <div id='home' className='app__header app__flex'>
+      <motion.div
+      whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+      transition={{ duration: 0.85, ease: 'easeOut' }}
+      className='app__header-info'>
+        <div className='app__header-badge'>
+          <div className='badge-cmp app__flex'>
+            <span>👋</span>
+            <div style={{marginLeft: 20}}>
+              <p className='p-text'>Hello, I am</p>
+              <h1 className='head-text'>Nicholas</h1>
+            </div>
+            </div>
+
+            <div className='tag-cmp app__flex'>
+              <p className='p-text'>Software Developer</p>
+              <p className='p-text'>Freelancer</p>
+              <p className='p-text'>Open Source Contributor</p>
+            </div>
+        </div>
+      </motion.div>
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.85, ease: 'easeOut', displayChilren: 0.5 }}
+        className='app__header-img'
+      >
+        <img src={images.klausbg} alt='profile_bg' />
+        <motion.img
+        whileInView={{scale: [0, 1]}}
+        transition={{ duration: 1, ease: 'easeInOut' }}
+        src={images.circle}
+        alt='profile_circle'
+        className='overlay_circle'
+        />
+
+      </motion.div>
+
+      <motion.div
+      variant={scaleVariant}
+      whileInView={scaleVariant.whileInView}
+      className='app__header-circles'
+      >
+        {[images.flutter, images.redux, images.sass].map((circle, index) => (
+          <div className='circle-cmp app__flex' key={`circle-${index}`}>
+            <img src={circle} alt='circle' />
+          </div>
+        ))}
+
+      </motion.div>
+
+    </div>
   )
 }
 
