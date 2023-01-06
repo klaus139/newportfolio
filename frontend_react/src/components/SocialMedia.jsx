d0f4255ce7e10da5b09b872ba14726923abe0c83
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2023-01-06 13:00:04 +0100

    finished the testimonial component

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -6,19 +6,29 @@ const SocialMedia = () => {
   return (
     <div className="app__social">
         <div>
+            <a href="https://www.twitter.com/Nick_k1aus" target="_blank" rel="noreferrer">
             <BsTwitter />
+            </a>
         </div>
         <div>
+            <a href="https://www.facebook.com/nicholas-igunbor" target='blank' rel='noreferrer'>
             <FaFacebookF />
+            </a>
         </div>
         <div>
+            <a href="https://www.instagram.com/nick_k1aus" target='blank' rel='noreferrer'>
             <BsInstagram />
+            </a>
         </div>
         <div>
+            <a href="https://www.linkedin.com/in/nicholasigunbor" target='blank' rel='noreferrer'>
             <FaLinkedinIn />
+            </a>
         </div>
         <div>
+            <a href="https://www.github.com/klaus139" target='blank' rel='noreferrer'>
             <FaGithub />
+            </a>
         </div>
     </div>
   )
--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -47,16 +47,16 @@ const Skills = () => {
           ))}
         </motion.div>
         <div className="app__skills-exp">
-          {experiences.map((experience) => (
+          {experiences.map((experience, index) => (
             <motion.div
               className="app__skills-exp-item"
-              key={experience.year}
+              key={experience.year + index}
             >
               <div className="app__skills-exp-year">
                 <p className="bold-text">{experience.year}</p>
               </div>
               <motion.div className="app__skills-exp-works">
-                {experience.works.map((work) => (
+                {experience.works.map((work, index) => (
                   <>
                     <motion.div
                       whileInView={{ opacity: [0, 1] }}
@@ -64,7 +64,7 @@ const Skills = () => {
                       className="app__skills-exp-work"
                       data-tip
                       data-for={work.name}
-                      key={work.name}
+                      key={work.name + index}
                     >
                       <h4 className="bold-text">{work.name}</h4>
                       <p className="p-text">{work.company}</p>
